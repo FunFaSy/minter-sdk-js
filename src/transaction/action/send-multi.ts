2ca147c9d9e15e26389f495d635a6764407c01cc
EvgeniyTaburyanskiy <Evgeniy.Taburyanskiy@gmail.com>
2021-11-09 21:53:23 +0300

    multisend + test

---
+++ src/transaction/action/send-multi.ts
@@ -0,0 +1,43 @@
+import {TransactionType} from '../transaction';
+import {Action} from './base_action';
+import {RlpSchemaField} from '../../util/define-properties';
+import {SendAction} from './send';
+
+/**
+ *
+ */
+export interface MultiSendActionParams {
+    list: SendAction[];         //
+}
+
+/**
+ *
+ */
+export class MultiSendAction extends Action {
+    list: Buffer[];
+
+    constructor(params: MultiSendActionParams) {
+        // Convert params to Buffers
+        const _params = {
+            list: params.list // Pass values through to nonBinaryArrayTransform encoder function. @see rlpSchema method
+        };
+
+        super(_params);
+
+        this.txType = TransactionType.MULTISEND;
+    }
+
+    rlpSchema(): RlpSchemaField[] {
+        return [
+            {
+                name               : 'list',
+                allowZero          : false,
+                default            : Buffer.from([]),
+                allowNonBinaryArray: true,
+                nonBinaryArrayTransform(action) {
+                    return action.raw;
+                },
+            },
+        ];
+    }
+}
