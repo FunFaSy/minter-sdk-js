0577c9556d36de75f3427e62d4db0a6cbfe74b05
EvgeniyTaburyanskiy <Evgeniy.Taburyanskiy@gmail.com>
2021-11-26 18:54:03 +0300

    build

--- lib/constants.d.ts
+++ lib/constants.d.ts
@@ -1,10 +1,10 @@
 /**
  *
  44' means BIP44
- 491' means Minter BIP @see https://github.com/satoshilabs/slips/blob/ef6d7700cc/slip-0044.md
+ 60' means Eth 491' [means Minter BIP @see https://github.com/satoshilabs/slips/blob/ef6d7700cc/slip-0044.md]
  0' means wallet/account #0
  0 means ?
  0 means index 0
  */
-export declare const MINTER_DERIVATION_PATH = "m/44'/491'/0'/0";
+export declare const MINTER_DERIVATION_PATH = "m/44'/60'/0'/0";
 export declare const MINTER_LAST_BLOCK_HEIGHT = 999999999;
