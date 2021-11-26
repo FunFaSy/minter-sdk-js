1daf211e1771d5504f236e0a249648d314372f58
EvgeniyTaburyanskiy <Evgeniy.Taburyanskiy@gmail.com>
2021-11-26 18:37:51 +0300

    roll back hd keys derivation to 60

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,11 @@
 /**
  *
  44' means BIP44
- 491' means Minter BIP @see https://github.com/satoshilabs/slips/blob/ef6d7700cc/slip-0044.md
+ 60' means Eth 491' [means Minter BIP @see https://github.com/satoshilabs/slips/blob/ef6d7700cc/slip-0044.md]
  0' means wallet/account #0
  0 means ?
  0 means index 0
  */
-export const MINTER_DERIVATION_PATH = 'm/44\'/491\'/0\'/0';// '/0' - rest number will add Account Generator
+export const MINTER_DERIVATION_PATH = 'm/44\'/60\'/0\'/0';// '/0' - rest number will add Account Generator
 
 export const MINTER_LAST_BLOCK_HEIGHT = 999999999;
