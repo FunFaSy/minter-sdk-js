87edc747de8f42fab83048c2fe7d3424dc4c15b4
Azimuth0x28 <Evgeniy.Taburyanskiy@gmail.com>
2021-12-10 00:17:04 +0300

    ignore reports dir

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    preset: 'ts-jest',
+    preset         : 'ts-jest',
+    reporters      : ['default', 'jest-junit'],
     testEnvironment: 'node',
     collectCoverage: false,
-    verbose: true
+    verbose        : true,
 };
