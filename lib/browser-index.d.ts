5eafc5e424752b19cb1d519f3000e6a21c63c8e2
Azimuth0x28 <Evgeniy.Taburyanskiy@gmail.com>
2021-12-10 14:44:14 +0300

    minor import fix

--- lib/browser-index.d.ts
+++ lib/browser-index.d.ts
@@ -1,4 +1,4 @@
 /** @hidden @module */
+import 'error-polyfill';
 export * as keyStores from './key_stores/browser-index';
 export * from './common-index';
-import 'error-polyfill';
--- src/browser-index.ts
+++ src/browser-index.ts
@@ -1,7 +1,6 @@
 /** @hidden @module */
 
+import 'error-polyfill';
+
 export * as keyStores from './key_stores/browser-index';
 export * from './common-index';
-//export * from './browser-connect';
-
-import 'error-polyfill';
